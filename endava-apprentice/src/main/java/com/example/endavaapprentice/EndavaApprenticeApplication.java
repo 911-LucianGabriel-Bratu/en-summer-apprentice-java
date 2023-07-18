27ed1efe9b68085975343ca605e453e173077e7c
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2023-07-18 11:21:09 +0300

    add project generated from spring initializr

---
+++ endava-apprentice/src/main/java/com/example/endavaapprentice/EndavaApprenticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.endavaapprentice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndavaApprenticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndavaApprenticeApplication.class, args);
+	}
+
+}
