c07e2bd86f95b2194f961ebc745dc19b36f37fff
Lucian B <lucian.bratu@stud.ubbcluj.ro>
2023-07-20 11:21:13 +0300

    Add DTO which contains attributes from Event, EventType and Venue

---
+++ endava-apprentice/src/main/java/com/example/endavaapprentice/Model/DTOs/EventVenueEventTypeDTO.java
@@ -0,0 +1,83 @@
+package com.example.endavaapprentice.Model.DTOs;
+
+import com.example.endavaapprentice.Model.TicketCategory;
+import com.example.endavaapprentice.Model.Venue;
+
+import java.util.Date;
+import java.util.List;
+
+public class EventVenueEventTypeDTO {
+    private Long eventID;
+    private Venue venue;
+    private String eventTypeName;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private List<TicketCategory> ticketCategories;
+
+    public EventVenueEventTypeDTO(Long eventID, Venue venue, String eventTypeName, String description, Date startDate, Date endDate, List<TicketCategory> ticketCategories) {
+        this.eventID = eventID;
+        this.venue = venue;
+        this.eventTypeName = eventTypeName;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.ticketCategories = ticketCategories;
+    }
+
+    public Long getEventID() {
+        return eventID;
+    }
+
+    public void setEventID(Long eventID) {
+        this.eventID = eventID;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+
+    public String getEventTypeName() {
+        return eventTypeName;
+    }
+
+    public void setEventTypeName(String eventTypeName) {
+        this.eventTypeName = eventTypeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<TicketCategory> getTicketCategories() {
+        return ticketCategories;
+    }
+
+    public void setTicketCategories(List<TicketCategory> ticketCategories) {
+        this.ticketCategories = ticketCategories;
+    }
+}
